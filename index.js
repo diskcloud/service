bdf719ab1853e8ac6776b6305b139c244e54e0cd
严家辉 <crazymryan@gmail.com>
2024-06-23 23:39:30 +0800

    fix: Fix the issue of incorrect thumbnail after uploading 🐛

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 const createDirectories = () => {
   const dirs = [
     path.join(__dirname, 'provisional'),
-    path.join(__dirname, 'public', 'resource')
+    path.join(__dirname, 'resource')
   ];
   dirs.forEach((dir) => {
     if (!fs.existsSync(dir)) {
